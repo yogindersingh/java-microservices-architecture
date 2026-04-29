0fbd63e0489b9ef9bc24770c3ad09ec1c37869b0
yogindersingh <38501528+yogindersingh@users.noreply.github.com>
2026-04-30 00:19:17 +0530

    Changed package for controller

--- Accounts/src/main/java/com/learning/Controller/AccountsController.java
+++ Accounts/src/main/java/com/learning/Controller/AccountsController.java
@@ -1,4 +1,4 @@
-package com.learning.Controller;
+package com.learning.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
