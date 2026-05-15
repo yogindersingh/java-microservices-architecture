1ab0316e2d4e9bb6125b53a4c8c3548de890f9bb
yogindersingh <38501528+yogindersingh@users.noreply.github.com>
2026-05-16 00:37:28 +0530

    Added configserver using spring cloud config server and mapped the client using spring cloud client and return response based on default/qa/prod profile for contact-info and build-info

---
+++ spring-cloud-config-server/src/test/java/com/learning/spring_cloud_config_server/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.spring_cloud_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
