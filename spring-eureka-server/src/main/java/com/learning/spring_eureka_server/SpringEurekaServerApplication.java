a9a65751b447c49f7b47ca57fd35a0904bf7bc9a
yogindersingh <38501528+yogindersingh@users.noreply.github.com>
2026-05-22 00:55:24 +0530

    Added eureka server code changes and eureka client changes to accounts,loans and cards microservice

---
+++ spring-eureka-server/src/main/java/com/learning/spring_eureka_server/SpringEurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.learning.spring_eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEurekaServerApplication.class, args);
+	}
+
+}
---
+++ spring-eureka-server/src/test/java/com/learning/spring_eureka_server/SpringEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.spring_eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
