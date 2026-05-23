d8729acb7a0f3c1f6fa032a11f2579374fc59b4a
yogindersingh <38501528+yogindersingh@users.noreply.github.com>
2026-05-24 00:58:49 +0530

    code changes to build eureka server as docker image and docker compose changes to include eureka server

--- spring-eureka-server/src/test/java/com/learning/spring_eureka_server/SpringEurekaServerApplicationTests.java
+++ spring-eureka-server/src/test/java/com/learning/spring_eureka_server/SpringEurekaServerApplicationTests.java
@@ -3,7 +3,6 @@ package com.learning.spring_eureka_server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class SpringEurekaServerApplicationTests {
 
 	@Test
